fcffbc1260bfd9cccb4418943dcf77ea4dcaeb7c
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 15:58:41 +0900

    Update Guild.java

--- textRpg/src/textRpg/Guild.java
+++ textRpg/src/textRpg/Guild.java
@@ -28,8 +28,78 @@ public class Guild {
 		}
 	}
 	
-	Player getGuildUnit(int num) {
+	public Player getGuildUnit(int num) {
 		return guildList.get(num);
 	}
-
+	
+	public void guildMenu() {
+		while(true) {
+			System.out.println("=============== [길드관리] ================");
+			System.out.println("[1.길드 목록]\t\t[2.길드원 추가]\t\t[3.길드원 삭제]");
+			System.out.println("[4.파티원 교체]\t\t[5.정렬]\t\t\t[0.뒤로가기]");
+			int sel = GameManager.scan.nextInt();
+			
+			if(sel == 1) {
+				printGuildList();
+				
+			} else if(sel == 2) {
+				addUnit();
+				
+			} else if(sel == 3) {
+				removeUnit();
+				
+			} else if(sel == 4) {
+				changeParty();
+				
+			} else if(sel == 5) {
+				sortUnit();
+				
+			} else if(sel == 0) {
+				break;
+				
+			} else {
+				continue;
+			}
+		}
+	}
+	
+	private void printGuildStatus(int index) {
+		// TODO Auto-generated method stub
+		guildList.get(index).printStatus();
+	}
+	
+	private void printGuildList() {
+		System.out.println("============= [길드원] =================");
+		for(int i = 0; i < guildList.size(); i++) {
+			System.out.println("[" + (i + 1) + "번]");
+			System.out.print(" [이름 : " + guildList.get(i).name + "]");
+			System.out.print(" [레벨 : " + guildList.get(i).level + "]");
+			System.out.print(" [체력 : " + guildList.get(i).curHp);
+			System.out.println(" / " + guildList.get(i).maxHp + "]");
+			System.out.print("[공격력 : " + guildList.get(i).power + "]");
+			System.out.print(" [방어력 : " + guildList.get(i).def + "]");
+			System.out.println(" [파티중 : " + guildList.get(i).party + "]");
+		}
+		System.out.println("======================================");
+	}
+	
+	private void addUnit() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void removeUnit() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void changeParty() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void sortUnit() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
