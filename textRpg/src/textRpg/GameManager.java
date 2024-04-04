22e7a376277789c7cd117da460b430c944a22f6c
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 10:48:47 +0900

    Update GameManager.java

--- textRpg/src/textRpg/GameManager.java
+++ textRpg/src/textRpg/GameManager.java
@@ -1,5 +1,7 @@
 package textRpg;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,7 +9,56 @@ public class GameManager {
 	static Scanner scan = new Scanner(System.in);
 	static Random ran = new Random();
 	
+	Map<String, Stage> stageList = new HashMap<String, Stage>();
+	
 	static String nextStage = "";
 	String curStage = "";
+	
+	static boolean isRun = true;
+	
+	public GameManager() {
+		stageList.put("StageStart", new StageTitle());
+		stageList.put("BATTLE", new StageBattle());
+		stageList.put("LOBBY", new StageLobby());
+		stageList.put("SETTING", new StageSetting());
+		nextStage = "StageStart";
+	}
+	
+	public boolean changeStage() {
+		System.out.println("curStage : " + curStage);
+		System.out.println("nextStage : " + nextStage);
+		
+//		if (curStage.equals(nextStage))
+//			return true;
+		
+		curStage = nextStage;
+		Stage stage = stageList.get(curStage);
+		stage.init(); // 초기화
+		
+		while(isRun) {
+			boolean isUpdate = stage.update();
+			if (!isUpdate)
+				break;
+		}
+		
+		if(!isRun)
+			return false;
+		
+		if (nextStage.equals(""))
+			return false;
+		else
+			return true;
+	}
+	
+	public static int inputNumber() {
+		int number = -1;
+		try {
+			String input = scan.next();
+			number = Integer.parseInt(input);
+		} catch(Exception e) {
+			System.out.println("숫자를 입력하세요.");
+		}
+		return number;
+	}
 
 }
