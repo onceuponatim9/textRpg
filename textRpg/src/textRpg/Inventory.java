271648ad63b9b734b333c15abc43286d06322dbb
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 15:55:35 +0900

    Update Inventory.java

--- textRpg/src/textRpg/Inventory.java
+++ textRpg/src/textRpg/Inventory.java
@@ -1,5 +1,20 @@
 package textRpg;
 
 public class Inventory {
+	public void inventoryMenu() {
+		while(true) {
+			System.out.println("============ [인벤토리 메뉴] =============");
+			System.out.println("[1.착용] [2.판매] [0.뒤로가기]");
+			int sel = GameManager.scan.nextInt();
+			
+			if(sel == 1) {
+				
+			} else if(sel == 2) {
+				
+			} else if(sel == 3) {
+				break;
+			}
+		}
+	}
 
 }
