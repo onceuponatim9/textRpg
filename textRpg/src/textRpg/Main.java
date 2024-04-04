ccba00ba925d148ffd957b35c187284d78eac54e
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 12:08:57 +0900

    Update Main.java

--- textRpg/src/textRpg/Main.java
+++ textRpg/src/textRpg/Main.java
@@ -2,7 +2,31 @@ package textRpg;
 
 public class Main {
 	public static void main(String[] args) {
+		GameManager gm = new GameManager();
+		StageBattle battle = new StageBattle();
+		StageTitle game = new StageTitle();
 		
+		System.out.println("==== TEXT RPG ====");
+		
+		// 게임 시작
+		while(true) {
+			boolean isStart = game.update();
+			if(isStart) {
+				break;
+			}
+		}
+		
+		// 게임 준비
+		battle.init();
+		
+		// 게임 진행
+		while(true) {
+			boolean isRun = gm.changeStage();
+			if(!isRun)
+				break;
+		}
+		
+		System.out.println("game over-");
 	}
 
 }
