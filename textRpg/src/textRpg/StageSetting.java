3873c8caf920a7cc66e81ef3a21e5a8edf0a0f89
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 15:54:54 +0900

    Update StageSetting.java

--- textRpg/src/textRpg/StageSetting.java
+++ textRpg/src/textRpg/StageSetting.java
@@ -1,10 +1,35 @@
 package textRpg;
 
 public class StageSetting extends Stage {
+	UnitManager um = null;
+	Shop shop;
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		while(true) {
+			System.out.println("=============== [메인메뉴] ================");
+			System.out.println("[1.길드관리]\t\t[2.상점]\t\t[3.인벤토리]");
+			System.out.println("[4.저장]\t\t[5.로드]\t\t[0.종료]");
+			int sel = GameManager.scan.nextInt();
+			
+			if(sel == 1) {
+				
+			} else if(sel == 2) {
+				
+			} else if(sel == 3) {
+				
+			} else if(sel == 4) {
+				
+			} else if(sel == 5) {
+				
+			} else if(sel == 0) {
+				GameManager.nextStage = "";
+				break;
+				
+			} else {
+				continue;
+			}
+		}
 		return false;
 	}
 
