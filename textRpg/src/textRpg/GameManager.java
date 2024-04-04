70c942f5950d8b166f722ef9f719af1327c82517
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 10:42:40 +0900

    Create GameManager.java

---
+++ textRpg/src/textRpg/GameManager.java
@@ -0,0 +1,13 @@
+package textRpg;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameManager {
+	static Scanner scan = new Scanner(System.in);
+	static Random ran = new Random();
+	
+	static String nextStage = "";
+	String curStage = "";
+
+}
