9a52479ad2ee66ab4ee4977c725e7bf1d4ba46d0
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 15:58:04 +0900

    Update Player.java

--- textRpg/src/textRpg/Player.java
+++ textRpg/src/textRpg/Player.java
@@ -1,5 +1,38 @@
 package textRpg;
 
 public class Player extends Unit {
+	static Guild guild = new Guild();
+	static Inventory inven = new Inventory();
+	
+	private int money;
+	
+	public Player() {
+		
+	}
+
+	public Player(String name, int level, int hp, int power, int def, int exp) {
+		super(name, level, hp, power, def, exp);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void init() {
+		guild.setGuild();
+		money = 100; // 플레이어의 초기 잔고 설정
+	}
+
+	@Override
+	public void skill() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void inventoryMenu() {
+		inven.inventoryMenu();
+	}
+
+	public void printStatus() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
