541e479cbe96a19a005dd7786217c51559e9d5fd
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 15:54:13 +0900

    Update Monster.java

--- textRpg/src/textRpg/Monster.java
+++ textRpg/src/textRpg/Monster.java
@@ -1,5 +1,35 @@
 package textRpg;
 
 public abstract class Monster {
+	String name;
+	int curhp;
+	int maxhp;
+	int power;
+	
+	Monster() {
+		
+	}
+	
+	Monster(String name, int max, int power) {
+	    this.name = name;
+	    this.curhp = max;
+	    this.maxhp = max;
+	    this.power = power;
+	}
+	
+	void init(int max, int power) {
+		this.curhp = max;
+	    this.maxhp = max;
+	    this.power = power;
+	}
+	
+	void init(String name, int max, int power) {
+		this.name = name;
+	    this.curhp = max;
+	    this.maxhp = max;
+	    this.power = power;
+	}
+	
+	public abstract void skill();
 
 }
