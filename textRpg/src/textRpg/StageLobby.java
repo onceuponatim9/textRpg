0178f67ff93b67f829a590fe2dc1e2aad5251fde
onceuponatim9 <sjung8018a@gmail.com>
2024-04-04 12:10:27 +0900

    Update StageLobby.java

--- textRpg/src/textRpg/StageLobby.java
+++ textRpg/src/textRpg/StageLobby.java
@@ -4,7 +4,17 @@ public class StageLobby extends Stage {
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		System.out.println("=====[Lobby]=====");
+		System.out.println("[1.전투] [2.종료]");
+		int sel = GameManager.scan.nextInt();
+		
+		if(sel == 1) {
+			GameManager.nextStage = "BATTLE";
+			
+		} else if(sel == 2) {
+			GameManager.nextStage = "";
+		}
+		
 		return false;
 	}
 
