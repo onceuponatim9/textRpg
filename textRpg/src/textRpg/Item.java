a555f2c3f3c5834f09edec12cdd025d54afff73a
onceuponatim9 <sjung8018a@gmail.com>
2024-04-05 15:50:23 +0900

    Update Item.java

--- textRpg/src/textRpg/Item.java
+++ textRpg/src/textRpg/Item.java
@@ -1,6 +1,12 @@
 package textRpg;
 
 public class Item {
+	final static int WEAPON = 1;
+	final static int ACCESSORY = 2;
+	final static int CLOTHES = 3;
+	final static int ARMOUR = 4;
+	final static int SHOES = 5;
+	
 	int kind;
 	String name;
 	int power;
