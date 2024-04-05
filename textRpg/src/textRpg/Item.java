daf22b1e7a4687ed0e81f4bf5670797e24292421
onceuponatim9 <sjung8018a@gmail.com>
2024-04-05 12:18:43 +0900

    Update Item.java

--- textRpg/src/textRpg/Item.java
+++ textRpg/src/textRpg/Item.java
@@ -1,5 +1,16 @@
 package textRpg;
 
 public class Item {
+	int kind;
+	String name;
+	int power;
+	int price;
+	
+	public void setItem(int k, String n, int p, int pr) {
+		this.kind = k;
+		this.name = n;
+		this.power = p;
+		this.price = pr;
+	}
 
 }
