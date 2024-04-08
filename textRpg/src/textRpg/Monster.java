057dd2fa1ddcca4f7d8a2f6059fc6be8402049f8
onceuponatim9 <sjung8018a@gmail.com>
2024-04-08 12:02:01 +0900

    Update Monster.java

--- textRpg/src/textRpg/Monster.java
+++ textRpg/src/textRpg/Monster.java
@@ -2,8 +2,8 @@ package textRpg;
 
 public abstract class Monster {
 	String name;
-	int curhp;
-	int maxhp;
+	int curHp;
+	int maxHp;
 	int power;
 	
 	Monster() {
@@ -12,24 +12,38 @@ public abstract class Monster {
 	
 	Monster(String name, int max, int power) {
 	    this.name = name;
-	    this.curhp = max;
-	    this.maxhp = max;
+	    this.curHp = max;
+	    this.maxHp = max;
 	    this.power = power;
 	}
 	
 	void init(int max, int power) {
-		this.curhp = max;
-	    this.maxhp = max;
+		this.curHp = max;
+	    this.maxHp = max;
 	    this.power = power;
 	}
 	
 	void init(String name, int max, int power) {
 		this.name = name;
-	    this.curhp = max;
-	    this.maxhp = max;
+	    this.curHp = max;
+	    this.maxHp = max;
 	    this.power = power;
 	}
 	
 	public abstract void skill();
-
+	
+	public void attack(Player unit) {
+		unit.curHp -= power;
+		System.out.println("[" + name + "]이 " + "[" + unit.name + "]에게 " + power + "의 데미지를 입힙니다.");
+		
+		if(unit.curHp <= 0) {
+			unit.curHp = 0;
+			System.out.println("[" + unit.name + "]을 처치했습니다.");
+		}
+	}
+	
+	public void printData() {
+		System.out.println("[" + name + "] [" + curHp + "/" + maxHp + "] [" + power + "]");
+	}
+	
 }
