f6c6cf985e9c28e2063377a0cc73f4be70ffb435
onceuponatim9 <sjung8018a@gmail.com>
2024-04-09 15:15:27 +0900

    Update GameManager.java

--- textRpg/src/textRpg/GameManager.java
+++ textRpg/src/textRpg/GameManager.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class GameManager {
 	static Scanner scan = new Scanner(System.in);
@@ -14,8 +15,9 @@ public class GameManager {
 	
 	Map<String, Stage> stageList = new HashMap<String, Stage>();
 	
-	private Map<User, Player> map;
+	//private Map<User, Vector<Player>> map;
 	private UserManager um = null;
+	static User curUser;
 	
 	int log = -1;
 	
@@ -26,7 +28,7 @@ public class GameManager {
 	
 	GameManager() {
 		um = UserManager.getInstance();
-		map = um.cloneMap();
+		//map = um.cloneMap();
 	}
 	
 	public void init() {
@@ -117,7 +119,9 @@ public class GameManager {
 			return;
 		}
 		
-		System.out.println("로그인 성공");
+		curUser = um.getUserByLog(log);
+		
+		System.out.println(curUser.getName() + " 로그인 성공");
 	}
 	
 	public void logout() {
